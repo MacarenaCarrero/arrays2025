7157fbc74ab58cddc1ba7a88e8eb08ea9ab0ffc9
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-03-21 00:31:36 +0100

    arrays2025

--- js/scripts.js
+++ js/scripts.js
@@ -32,8 +32,52 @@ catalog('Falda');
 
 const listEmails = domain => {};
 
-listEmails([
+const emails = [
   'cliente1@gmail.com',
   'cliente2@hotmail.com',
   'cliente3@gmail.com'
-]);
+];
+
+if (
+  emails[0].includes('gmail') ||
+  emails[1].includes('gmail') ||
+  emails[2].includes('gmail')
+) {
+  console.log('Correo de Gmail encontrado');
+} else {
+  console.log('No hay correo Gmail.');
+}
+
+// 4 Macarena necesita generar un identificador único para cada usuario. Recibe un array con nombre y apellido, (por ejemplo: ['Enrique Sofresco', 'Esther Colero', 'Leandro Gado']) y debe imprimir las dos primeras letras de cada nombre y de cada apellido en mayúsculas, seguidas de un número aleatorio entre 10 y 99.
+
+const nameUser = () => {
+  const names = ['Enrique', 'Esther', 'Leandro'];
+  const surname = ['Sofresco', 'Colero', 'Gado'];
+
+  console.log(
+    names[0].substring(0, 2).toUpperCase() +
+      surname[0].substring(0, 2).toUpperCase() +
+      Math.floor(Math.random()) * (99 - 10) +
+      10
+  );
+  console.log(
+    names[1].substring(0, 2).toUpperCase() +
+      surname[1].substring(0, 2).toUpperCase() +
+      Math.floor(Math.random() * (99 - 10) + 10)
+  );
+};
+nameUser();
+
+// 5 Abby encontró una caja fuerte con un código de acceso en un array de cuatro dígitos, por ejemplo: [1, 5, 3, 8]. Si la suma del primer y el último número es par, imprimirá "Acceso concedido", si no, "Acceso denegado".
+
+const secretCode = code => {
+  if ((code[0] + code[3]) % 2 === 0) {
+    console.log('Acceso concedido.');
+  } else {
+    console.log('Acceso denegado.');
+  }
+};
+secretCode([2, 3, 6, 8]);
+secretCode([3, 6, 8, 2]);
+
+// 6 Camila quiere generar dos números aleatorios entre 1 y 100 y determinar cuál es el mayor. Después los guardará en un array poniendo en la primera posición el mayor y en segunda posición el menor. Si son iguales, imprimirá "Empate", si no, imprimirá el array.
